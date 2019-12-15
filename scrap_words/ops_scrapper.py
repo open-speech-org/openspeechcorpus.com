ffa06a04a41bb6889c4a238154a7b6f89460453b
ma0c <ma0@contraslash.com>
2019-12-15 16:22:58 -0500

    untested generic master detail extraction

--- scrap_words/ops_scrapper.py
+++ scrap_words/ops_scrapper.py
@@ -48,11 +48,13 @@ def create_file_with_name_and_content(file_path, content):
 
 class OPSScrapper(object):
     SENTENCES_URL_NAME = "sentences"
+    AUTHORS_URL_NAME = "authors"
     TALES_URL_NAME = "tales"
     TALE_URL_NAME = "tale"
 
     URLS = {
         SENTENCES_URL_NAME: "/api/sentences",
+        AUTHORS_URL_NAME: "/api/authors",
         TALES_URL_NAME: "/api/tales",
         TALE_URL_NAME: "/api/sentences/{}"
     }
@@ -70,25 +72,87 @@ class OPSScrapper(object):
     def get_url_for(self, url_name):
         return f"{base_url}{self.URLS[url_name]}"
 
+    def generic_master_detail_scrap(
+            self,
+            master_url,
+            detail_url,
+            master_output_path,
+            detail_output_path,
+            entity_name,
+            master_response_node_with_detail_info,
+            master_output_name="index.json",
+            attribute_to_extract_and_name_detail="id"
+    ):
+        """
+        This functions has utilities to scrap an entity and its related detail
+        :param master_url: URL for  master
+        :param detail_url: URL for detail
+        :param master_output_path: output to store the master
+        :param detail_output_path: output to store the detail
+        :param entity_name: Entity name for logging purposes
+        :param master_response_node_with_detail_info: Node inside the master response with the detail info
+        :param master_output_name: file to store master response
+        :param attribute_to_extract_and_name_detail: attribute from the master response to query detail
+        :return:
+        """
+        master_response = requests.get(master_url)
+        if master_response.ok:
+            master_json_content = master_response.json()
+            create_dir_if_does_not_exists(master_output_path)
+            create_file_with_name_and_content(
+                os.path.join(master_output_path, master_output_name),
+                master_response.content.decode()
+            )
+            LOGGER.info(f"{entity_name} fetched")
+            LOGGER.debug(master_json_content)
+            for detail in master_json_content.get(master_response_node_with_detail_info, []):
+                detail_identifier = str(detail.get(attribute_to_extract_and_name_detail))
+                detail_response = requests.get(detail_url.format(detail_identifier))
+                if detail_response.ok:
+                    detail_decoded_content = detail_response.content.decode()
+                    create_file_with_name_and_content(
+                        os.path.join(detail_output_path, detail_identifier),
+                        detail_decoded_content
+                    )
+                    LOGGER.info(f"{entity_name} with id {detail_identifier} with status OK")
+                    LOGGER.debug(detail_decoded_content)
+                else:
+                    LOGGER.error(
+                        f"Error calling {detail_url}, http error code: {detail_response.status_code}"
+                    )
+
+    def scrap_authors(self):
+        """
+        This function scraps data from the authors api
+        :return:
+        """
+
     def scrap_tales_sentences(self):
         """
         This function scraps data from the tales api
-        :param base_url: URL from OPS
-        :param output_folder: Folder to store all output
         :return: None
         """
         url_for_all_tales = self.get_url_for(self.TALES_URL_NAME)
         tales_response = requests.get(url_for_all_tales)
+        url_for_tale = self.get_url_for(self.TALE_URL_NAME)
+        tales_dir = os.path.join(self.api_folder, "tales")
+        sentences_dir = os.path.join(self.api_folder, "sentences")
+        self.generic_master_detail_scrap(
+            url_for_all_tales,
+            url_for_tale,
+            tales_dir,
+            sentences_dir,
+            "Tales",
+            "tales"
+        )
         if tales_response.ok:
             tales = tales_response.json()
-            tales_dir = os.path.join(self.api_folder, "tales")
             create_dir_if_does_not_exists(tales_dir)
             create_file_with_name_and_content(os.path.join(tales_dir, "index.json"), tales_response.content.decode())
             LOGGER.info("Tales fetched")
             LOGGER.debug(tales)
             for tale in tales.get("tales", list()):
                 tale_id = str(tale.get("id", 0))
-                url_for_tale = self.get_url_for(self.TALE_URL_NAME).format(tale_id)
                 single_tale_response = requests.get(url_for_tale)
                 if single_tale_response.ok:
                     create_file_with_name_and_content(
